d03e2bc44bb1a3a27076eab8437085849c28f723
Azat Bahawi <azahi@teknik.io>
2020-10-05 09:01:24 +0300

    Removing unecessary stuff

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -5,6 +5,7 @@ c.InteractiveShell.colors = 'Neutral'
 c.InteractiveShell.confirm_exit = False
 c.InteractiveShell.editor = 'vim'
 c.InteractiveShell.xmode = 'Context'
-c.InteractiveShellApp.log_level = 20
 c.PrefilterManager.multi_line_specials = True
-c.TerminalIPythonApp.display_banner = True
+c.TerminalIPythonApp.display_banner = False
+
+# flake8: noqa
