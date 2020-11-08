86fdf9ccdcfc17736562ad45bd897215e0151224
Azat Bahawi <azahi@teknik.io>
2020-11-08 22:52:03 +0300

    just shoot me

--- profile/.profile.d/ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if command -v ssh >/dev/null 2>&1
-then
-    alias s="TERM=xterm ssh"
-fi
---
+++ profile/.profile.d/wakatime.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if command -v wakatime >/dev/null 2>&1
+then
+    export WAKATIME_HOME="${HOME}/.wakatime"
+
+    [ ! -d "${WAKATIME_HOME}" ] && \
+        mkdir -p "${WAKATIME_HOME}"
+fi
