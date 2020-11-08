cddb3b2508d0375c9874ac2f133ace712d4bc923
Azat Bahawi <azahi@teknik.io>
2020-11-08 23:30:16 +0300

    Landed.

--- profile/.profile.d/wget.sh
+++ profile/.profile.d/wget.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if command -v wget >/dev/null 2>&1
+if command -v wget >/dev/null 2>&1 \
+&& [ "$(hostname)" != "sava" ]
 then
     # shellcheck disable=SC2139
     alias wget="wget --hsts-file=${XDG_DATA_HOME}/wget-hsts"
