375de5305ab9370023181d2dc22f1619935fb2a1
Azat Bahawi <azahi@teknik.io>
2020-12-13 19:33:27 +0300

    this should fix it

--- profile/.profile.d/docker.sh
+++ profile/.profile.d/docker.sh
@@ -2,11 +2,12 @@
 
 if command -v docker >/dev/null 2>&1
 then
-    alias d-ip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
-    alias d-pid="docker inspect --formet '{{ .State.Pid }}'"
-    alias d-rm-all="docker rm \$(docker ps --quiet --all)"
-    alias d-rm-exited="docker rm \$(docker ps --quiet --formet 'status=exited')"
-    alias d-rmi-all="docker rmi \$(docker images --quiet --all)"
-    alias d-rmi-dangling="docker rmi \$(docker images --quiet --format 'dangling=true')"
-    alias d-stop-all="docker stop \$(docker ps --quiet --format 'status=running')"
+    alias dk="docker"
+    alias dk-ip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+    alias dk-pid="docker inspect --formet '{{ .State.Pid }}'"
+    alias dk-rm-all="docker rm \$(docker ps --quiet --all)"
+    alias dk-rm-exited="docker rm \$(docker ps --quiet --formet 'status=exited')"
+    alias dk-rmi-all="docker rmi \$(docker images --quiet --all)"
+    alias dk-rmi-dangling="docker rmi \$(docker images --quiet --format 'dangling=true')"
+    alias dk-stop-all="docker stop \$(docker ps --quiet --format 'status=running')"
 fi
--- zsh/.zlogout
+++ zsh/.zlogout
@@ -4,6 +4,9 @@ if [ -f "${HOME}/.bin/logout-hook" ]
 then
     source "${HOME}/.bin/logout-hook"
 
-    #rm_existing "${HOME}"/.*.zwc.old
-    #rm_existing "${HOME}"/.zsh.d/*.zwc.old
+    rm_existing "${ZLOGIN}.zwc.old"
+    rm_existing "${ZLOGOUT}.zwc.old"
+    rm_existing "${ZPROFILE}.zwc.old"
+    rm_existing "${ZSHENV}.zwc.old"
+    rm_existing "${ZSHRC}.zwc.old"
 fi
