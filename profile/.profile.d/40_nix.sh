2642589ea605a1aa7fdb96e0695ebab5f0b1810f
Azat Bahawi <azahi@teknik.io>
2021-04-10 14:20:19 +0300

    Updated build targets.

--- profile/.profile.d/40_nix.sh
+++ profile/.profile.d/40_nix.sh
@@ -5,6 +5,8 @@ nixsh="${HOME}/.nix-profile/etc/profile.d/nix.sh"
 if [ -e "${nixsh}" ]; then
     # shellcheck disable=SC1090
     . "${nixsh}"
+
+    export NIXPKGS_ALLOW_UNFREE="1"
 fi
 
 unset nixsh
