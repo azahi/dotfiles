a5bd91005baa827b38a970609550d7b833b2be08
Azat Bahawi <azahi@teknik.io>
2021-04-19 23:55:02 +0300

    Trying to fake a conflict

--- profile/.profile.d/00_lang.sh
+++ profile/.profile.d/00_lang.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if [ -z "$LANG" ]; then
-    export LANG="en_US.UTF-8"
-fi
-
 export LC_COLLATE=C
+export LC_CTYPE=C
+export LC_MESSAGES=C
--- profile/.profile.d/40_nix.sh
+++ profile/.profile.d/40_nix.sh
@@ -7,6 +7,12 @@ if [ -e "${nixsh}" ]; then
     . "${nixsh}"
 
     export NIXPKGS_ALLOW_UNFREE="1"
+
+    hm_vars="$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    if [ -e "${hm_vars}" ]; then
+        . "${hm_vars}"
+    fi
+    unset hm_vars;
 fi
 
 unset nixsh
