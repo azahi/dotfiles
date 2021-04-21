81c0dafb0aa1ac578982808cddf665dc57a53516
Azat Bahawi <azahi@teknik.io>
2021-04-21 14:45:59 +0300

    fixed some minor stuff, might need some additional work.

---
+++ nix/.local/etc/nixpkgs/home.nix
@@ -0,0 +1,388 @@
+{ config, pkgs, ... }:
+let
+  username = "azahi";
+  homeDirectory = "/home/${username}";
+  localDirectory = "${homeDirectory}/.local";
+in {
+  home = {
+    username = username;
+    homeDirectory = homeDirectory;
+
+    packages = with pkgs; [
+      bat
+      calibre
+      fd
+      filezilla
+      hledger
+      jetbrains.clion
+      pandoc
+      ripgrep
+      toilet
+    ];
+
+    stateVersion = "21.05";
+  };
+
+  xdg = {
+    cacheHome = "${localDirectory}/var/cache";
+    configHome = "${localDirectory}/etc";
+    dataHome = "${localDirectory}/var/lib";
+  };
+
+  programs = {
+    home-manager = { enable = true; };
+
+    direnv = {
+      enable = true;
+      # TODO Test
+
+      enableNixDirenvIntegration = true;
+    };
+
+    neovim = {
+      enable = false;
+      # TODO Fix that error
+
+      extraPackages = with pkgs; [
+        ccls
+        clang
+        editorconfig-core-c
+        gopls
+        haskell-language-server
+        nixfmt
+        pipenv
+        poetry
+        shellcheck
+        shfmt
+        texlab
+        wakatime
+      ];
+      extraPython3Packages = (ps:
+        with ps; [
+          autopep8
+          black
+          isort
+          mccabe
+          nose
+          pip
+          pyflakes
+          pylint
+          pyls-black
+          pyls-isort
+          pytest
+          python-language-server
+          setuptools
+          virtualenv
+        ]);
+      plugins = with pkgs.vimPlugins; [
+        SyntaxRange
+        auto-pairs
+        calendar-vim
+        csv-vim
+        editorconfig-vim
+        gentoo-syntax
+        nginx-vim
+        rainbow_parentheses
+        vim-abolish
+        vim-commentary
+        vim-cool
+        vim-eunuch
+        vim-expand-region
+        vim-fugitive
+        vim-gnupg
+        vim-husk
+        vim-illuminate
+        vim-lastplace
+        vim-nix
+        vim-obsession
+        vim-polyglot
+        vim-python-pep8-indent
+        vim-repeat
+        vim-rhubarb
+        vim-shellcheck
+        vim-sleuth
+        vim-speeddating
+        vim-surround
+        vim-tmux
+        vim-tmux-focus-events
+        vim-unimpaired
+        neco-vim
+        neoinclude
+        vim-wakatime
+        {
+          plugin = vim-sneak;
+          config = ''
+            let g:sneak#label = 1
+
+            nmap f <Plug>Sneak_f
+            nmap F <Plug>Sneak_F
+            vmap f <Plug>Sneak_f
+            vmap F <Plug>Sneak_F
+
+            nmap t <Plug>Sneak_t
+            nmap T <Plug>Sneak_T
+            vmap t <Plug>Sneak_t
+            vmap T <Plug>Sneak_T
+
+            nmap <Plug>(Go_away_Sneak_s) <Plug>Sneak_s
+            nmap <Plug>(Go_away_Sneak_S) <Plug>Sneak_S
+          '';
+        }
+        {
+          plugin = supertab;
+          config = ''
+            let g:SuperTabDefaultCompletionType = '<C-n>'
+          '';
+        }
+        {
+          plugin = vim-clang-format;
+          config = ''
+            nnoremap <Buffer><Leader>cf :<C-u>ClangFormat<CR>
+            vnoremap <Buffer><Leader>cf :<C-u>ClangFormat<CR>
+          '';
+        }
+        {
+          plugin = LanguageClient-neovim;
+          config = ''
+            let g:LanguageClient_loadSettings = 1
+
+            nnoremap <Leader>lm :<C-u>call LanguageClient_contextMenu()<CR>
+            nnoremap <Leader>la :<C-u>call LanguageClient_workspace_applyEdit()<CR>
+            nnoremap <Leader>lc :<C-u>call LanguageClient#textDocument_completion()<CR>
+            nnoremap <Leader>ld :<C-u>call LanguageClient#textDocument_definition()<CR>
+            nnoremap <Leader>le :<C-u>call LanguageClient#textDocument_references()<CR>
+            nnoremap <Leader>lf :<C-u>call LanguageClient#textDocument_formatting()<CR>
+            nnoremap <Leader>lh :<C-u>call LanguageClient#textDocument_hover()<CR>
+            nnoremap <Leader>lr :<C-u>call LanguageClient#textDocument_rename()<CR>
+            nnoremap <Leader>ls :<C-u>call LanguageClient#textDocument_documentSymbol()<CR>
+            nnoremap <Leader>lt :<C-u>call LanguageClient#textDocument_typeDefinition()<CR>
+
+            let g:LanguageClient_serverCommands = {}
+
+            if executable('ccls')
+                let g:LanguageClient_serverCommands[ 'c'   ] = [ 'ccls' ]
+                let g:LanguageClient_serverCommands[ 'cpp' ] = [ 'ccls' ]
+
+                nnoremap <Leader>h :<C-u>call LanguageClient#findLocations({'method':'$ccls/navigate','direction':'L'})<CR>
+                nnoremap <Leader>j :<C-u>call LanguageClient#findLocations({'method':'$ccls/navigate','direction':'D'})<CR>
+                nnoremap <Leader>k :<C-u>call LanguageClient#findLocations({'method':'$ccls/navigate','direction':'U'})<CR>
+                nnoremap <Leader>l :<C-u>call LanguageClient#findLocations({'method':'$ccls/navigate','direction':'R'})<CR>
+            elseif executable('clangd')
+                let g:LanguageClient_serverCommands[ 'c'   ] = [ 'clangd', '-background-index' ]
+                let g:LanguageClient_serverCommands[ 'cpp' ] = [ 'clangd', '-background-index' ]
+            endif
+
+            if executable('gopls')
+                let g:LanguageClient_serverCommands[ 'go' ] = [ 'gopls' ]
+            endif
+
+            if executable('haskell-language-server-wrapper')
+                let g:LanguageClient_serverCommands[ 'haskell' ] = [ 'haskell-language-server-wrapper', '--lsp' ]
+            endif
+
+            if executable('pyls')
+                let g:LanguageClient_serverCommands[ 'python' ] = [ 'pyls' ]
+            endif
+          '';
+        }
+      ];
+
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+
+      withNodeJs = false;
+      withPython = false;
+      withPython3 = true;
+      withRuby = false;
+    };
+
+    qutebrowser = {
+      enable = false;
+      # TODO
+
+      keyBindings = {
+        normal = { "z" = "hint links spawn --detach mpv {hint-url}"; };
+      };
+      searchEngines = {
+        d = "https://duckduckgo.com/?q={}'";
+        g = "https://www.google.com/search?q={}";
+        aw = "https://wiki.archlinux.org/?search={}";
+        nw = "https://nixos.wiki/index.php?search={}";
+      };
+    };
+
+    readline = {
+      enable = false;
+      # TODO Test
+
+      bindings = {
+        "$" = "end-of-line";
+        "/" = "forward-search-history";
+        "?" = "reverse-search-history";
+        "G" = "end-of-history";
+        "\\C-a" = "beginning-of-line";
+        "\\C-e" = "end-of-line";
+        "\\C-l" = "clear-screen";
+        "\\C-n" = "history-search-forward";
+        "\\C-p" = "history-search-backward";
+        "\\C-w" = "backward-kill-word";
+        "\\t" = "menu-complete";
+        "^" = "beginning-of-line";
+        "b" = "backward-word";
+        "gg" = "beginning-of-history";
+        "j" = "history-search-forward";
+        "k" = "history-search-backward";
+        "w" = "forward-word";
+        "x" = "delete-char";
+      };
+      variables = {
+        bell-style = "visible";
+        bind-tty-special-chars = false;
+        blink-matching-paren = true;
+        colored-completitrue-prefix = true;
+        colored-stats = true;
+        completitrue-ignore-case = true;
+        ctruevert-meta = false;
+        echo-ctruetrol-characters = false;
+        editing-mode = "vi";
+        input-meta = true;
+        mark-symlinked-directories = true;
+        match-hidden-files = false;
+        menu-complete-display-prefix = true;
+        meta-flag = true;
+        output-meta = true;
+        page-completitrues = true;
+        show-all-if-ambiguous = true;
+        show-all-if-unmodified = true;
+        show-mode-in-prompt = true;
+        visible-stats = true;
+      };
+      extraConfig = ''
+        $if term=linux
+          set vi-ins-mode-string \1\e[?0c\2
+          set vi-cmd-mode-string \1\e[?8c\2
+        $else
+          set vi-ins-mode-string \1\e[6 q\2
+          set vi-cmd-mode-string \1\e[2 q\2
+        $endif
+      '';
+    };
+
+    ssh = {
+      enable = false;
+
+      compression = true;
+      controlMaster = "auto";
+      controlPath = "${homeDirectory}/.ssh/%r@%h:%p.sock";
+      controlPersist = "24H";
+      hashKnownHosts = true;
+      serverAliveCountMax = 30;
+      serverAliveInterval = 60;
+      matchBlocks = {
+        # TODO Add masked work hosts?
+        "github" = {
+          hostname = "github.com";
+          user = "git";
+        };
+        "gitlab" = {
+          hostname = "gitlab.com";
+          user = "git";
+        };
+        "bitbucket" = {
+          hostname = "bitbucket.com";
+          user = "git";
+        };
+        "azahi" = {
+          hostname = "azahi.cc";
+          user = "azahi";
+          port = 22022;
+        };
+        "yavanna" = {
+          hostname = "yavanna.azahi.cc";
+          user = "azahi";
+          port = 22022;
+        };
+      };
+    };
+
+    tmux = {
+      enable = false;
+
+      aggressiveResize = true;
+      baseIndex = 1;
+      clock24 = true;
+      disableConfirmationPrompt = true;
+      escapeTime = 0;
+      keyMode = "vi";
+      newSession = true;
+      resizeAmount = 10;
+      shortcut = "b";
+      terminal = "screen-256color";
+
+      # TODO
+    };
+
+    vscode = {
+      enable = false;
+      # TODO
+
+      haskell = {
+        enable = true;
+        hie = {
+          enable = false;
+          # TODO
+        };
+      };
+    };
+
+    zathura = {
+      enable = false;
+      # TODO
+    };
+  };
+
+  services = {
+    # TODO Test on systemd
+    dunst = {
+      enable = false;
+      # TODO
+    };
+
+    dwm-status = {
+      enable = false;
+      # TODO
+    };
+
+    emacs = {
+      enable = false;
+      # TODO
+    };
+
+    gpg-agent = {
+      enable = false;
+      # TODO Test
+
+      enableSshSupport = true;
+
+      defaultCacheTtl = 999999;
+      defaultCacheTtlSsh = 999999;
+      maxCacheTtl = 999999;
+      maxCacheTtlSsh = 999999;
+
+      grabKeyboardAndMouse = true;
+
+      pinentryFlavor = "qt";
+    };
+
+    mbsync = {
+      enable = false;
+      # TODO
+    };
+
+    mpd = {
+      enable = false;
+      # TODO
+    };
+  };
+}
