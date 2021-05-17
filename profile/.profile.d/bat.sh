a0d85750993127b30843a2d480dbaa8ebd85e016
Azat Bahawi <azahi@teknik.io>
2021-05-17 11:07:40 +0300

    Removing unecessary stuff

---
+++ profile/.profile.d/bat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if command -v bat >/dev/null 2>&1; then
+	export BAT_THEME="base16"
+
+	if [ "${MANPAGER}" == "bat" ] && ! command -v batman >/dev/null 2>&1; then
+		export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+	fi
+fi
+
