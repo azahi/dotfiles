2014ef0323c3152ccf7961b9735884c0510ddaaf
Azat Bahawi <azahi@teknik.io>
2021-06-30 01:16:44 +0300

    I cannot believe that it took this long to write a test for this.

--- profile/.profile.d/vi.sh
+++ profile/.profile.d/vi.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 _implementations="
-nvim
 vim
 vi
 "
@@ -9,15 +8,10 @@ vi
 for _implementation in ${_implementations}; do
 	# shellcheck disable=SC2139
 	command -v "${_implementation}" >/dev/null 2>&1 &&
-		alias nvim="${_implementation}" &&
 		alias vim="${_implementation}" &&
 		alias vi="${_implementation}" &&
 		alias v="${_implementation}" &&
 		break
 done
-
-if [ "${_implementation}" = "nvim" ]; then
-	export NVIM_TUI_ENABLE_TRUE_COLOR="1"
-fi
 
 unset _implementation _implementations
